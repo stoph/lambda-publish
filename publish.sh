95a966f8666d228e7295e5dba49ff520bde80be7
Stoph <christoph.khouri@gmail.com>
2016-11-28 18:51:01 -0500

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,27 @@
+skill=$1
+
+if [[ -n "$skill" ]]; then
+
+	if [[ -d "$skill" ]]; then
+
+		echo -e "\033[1;31m Building $skill \033[0m"
+		if [[ -f "index.zip" ]]; then
+			echo "   Deleting existing 'index.zip'..."
+			rm index.zip
+		fi
+
+		cd $skill
+		echo "   Zipping files..."
+		zip -rq ../index.zip *
+		cd ..
+		echo "   Pushing to Lamba..."
+		aws lambda update-function-code --function-name latin-lunch --zip-file fileb://index.zip
+
+		echo -e "\033[1;31m Done \033[0m"
+	else
+		echo "$skill does not exist."
+	fi
+
+else
+	echo "Usage: ./publish.sh <skill_name_directory>"
+fi
